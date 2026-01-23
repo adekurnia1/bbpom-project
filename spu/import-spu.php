44835da3ca2db1e0c5f4d51e42d6dee81cabdf0f
Adkurrr <belajarpemrograman13@gmail.com>
2026-01-23 14:26:59 +0700

    update: fix error penanggalan pada import spu

--- spu/import-spu.php
+++ spu/import-spu.php
@@ -7,6 +7,13 @@ if (!isset($_SESSION['ssLogin'])) {
     exit;
 }
 
+// fungsi taruh di luar loop
+function fixDate($tgl)
+{
+    if (empty($tgl)) return null;
+    return date('Y-m-d', strtotime(str_replace('/', '-', trim($tgl))));
+}
+
 if (isset($_POST['import'])) {
 
     $file = $_FILES['file_csv']['tmp_name'];
@@ -22,12 +29,18 @@ if (isset($_POST['import'])) {
             $tipe_sampel    = mysqli_real_escape_string($koneksi, $data[1]);
             $asal_sampling  = mysqli_real_escape_string($koneksi, $data[2]);
             $bulan_masuk    = mysqli_real_escape_string($koneksi, $data[3]);
-            $tgl_masuk_lab  = mysqli_real_escape_string($koneksi, $data[4]);
-            $tgl_spk        = mysqli_real_escape_string($koneksi, $data[5]);
+
+            // KONVERSI DULU BARU ESCAPE
+            $tgl_masuk_lab  = fixDate($data[4]);
+            $tgl_spk        = fixDate($data[5]);
+
+            $tgl_masuk_lab  = mysqli_real_escape_string($koneksi, $tgl_masuk_lab);
+            $tgl_spk        = mysqli_real_escape_string($koneksi, $tgl_spk);
+
             $jumlah_sampel  = mysqli_real_escape_string($koneksi, $data[6]);
             $timeline       = mysqli_real_escape_string($koneksi, $data[7]);
 
-            // cegah duplikat parameter
+            // cegah duplikat
             $cek = mysqli_query($koneksi, "
                 SELECT 1 FROM tbl_spu
                 WHERE no_spu='$no_spu'
@@ -37,11 +50,11 @@ if (isset($_POST['import'])) {
 
                 mysqli_query($koneksi, "
                     INSERT INTO tbl_spu
-                    (no_spu, tipe_sampel, asal_sampling, bulan_masuk, tgl_masuk_lab, tgl_spk,
-                    jumlah_sampel, timeline)
+                    (no_spu, tipe_sampel, asal_sampling, bulan_masuk, 
+                     tgl_masuk_lab, tgl_spk, jumlah_sampel, timeline)
                     VALUES
-                    ('$no_spu','$tipe_sampel','$asal_sampling','$bulan_masuk','$tgl_masuk_lab','$tgl_spk',
-                    '$jumlah_sampel', '$timeline')
+                    ('$no_spu','$tipe_sampel','$asal_sampling','$bulan_masuk',
+                     '$tgl_masuk_lab','$tgl_spk','$jumlah_sampel','$timeline')
                 ");
             }
         }
@@ -49,7 +62,7 @@ if (isset($_POST['import'])) {
         fclose($handle);
 
         echo "<script>
-            alert('Import parameter uji berhasil');
+            alert('Import SPU berhasil');
             window.location='list-spu.php';
         </script>";
     }
