ff206d4760c5c92dc442d216a0dc65981518bd20
Adkurrr <belajarpemrograman13@gmail.com>
2026-02-12 15:11:34 +0700

    update: update proses-terima-penguji.php

--- sampel/proses-terima-penguji.php
+++ sampel/proses-terima-penguji.php
@@ -5,28 +5,49 @@ require_once "../vendor/autoload.php";
 
 use Dompdf\Dompdf;
 
-$id_pengiriman = $_POST['id_pengiriman'];
+if (!isset($_SESSION["ssLogin"])) {
+    header("location: ../auth/login.php");
+    exit;
+}
+
+$id_pengiriman = mysqli_real_escape_string($koneksi, $_POST['id_pengiriman']);
 $id_penguji    = $_SESSION['ssId'];
 
-/* UPDATE STATUS */
-mysqli_query($koneksi, "
-    UPDATE tbl_pengiriman_sampel
-    SET status_pengiriman='diterima'
+/* CEK DATA VALID */
+$cek = mysqli_query($koneksi, "
+    SELECT * FROM tbl_pengiriman_sampel
     WHERE id_pengiriman='$id_pengiriman'
       AND id_penguji='$id_penguji'
 ");
 
-/* AMBIL DATA */
+if (mysqli_num_rows($cek) == 0) {
+    header("location:inbox-sampel-penguji.php?msg=invalid");
+    exit;
+}
+
+$pengiriman = mysqli_fetch_assoc($cek);
+
+/* UPDATE STATUS */
+mysqli_query($koneksi, "
+    UPDATE tbl_pengiriman_sampel
+    SET status_pengiriman='diterima',
+        status_uji='proses',
+        tgl_terima=NOW()
+    WHERE id_pengiriman='$id_pengiriman'
+");
+
+/* AMBIL DATA LENGKAP UNTUK PDF */
 $q = mysqli_query($koneksi, "
     SELECT 
-        ps.*, s.*,
+        s.*, 
+        ps.file_spp,
         u1.nama AS nama_penyelia,
         u2.nama AS nama_penguji,
         sp.no_spu,
         sp.tgl_spk,
         sp.timeline
-    FROM tbl_pengiriman_sampel ps
-    JOIN tbl_sampel s ON s.no_spl_sipt = ps.no_spl_sipt
+    FROM tbl_sampel s
+    JOIN tbl_pengiriman_sampel ps ON ps.no_spl_sipt = s.no_spl_sipt
     JOIN tbl_users u1 ON u1.id_user = ps.id_penyelia
     JOIN tbl_users u2 ON u2.id_user = ps.id_penguji
     JOIN tbl_spu sp ON sp.no_spu = s.no_spu
@@ -35,7 +56,7 @@ $q = mysqli_query($koneksi, "
 
 $data = mysqli_fetch_assoc($q);
 
-/* GENERATE PDF BARU (v2) */
+/* GENERATE PDF v2 (SETELAH DITANDATANGANI PENGUJI) */
 ob_start();
 include "../doc_templates/template_spp.php";
 $html = ob_get_clean();
@@ -45,8 +66,9 @@ $dompdf->loadHtml($html);
 $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
 
-/* OVERWRITE FILE */
-file_put_contents("../file_spp/".$data['file_spp'], $dompdf->output());
+/* OVERWRITE FILE SPP */
+$filePath = "../file_spp/" . $data['file_spp'];
+file_put_contents($filePath, $dompdf->output());
 
 header("location:inbox-sampel-penguji.php?msg=diterima");
 exit;
